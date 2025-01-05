2ab2a19b440e07eee60625915d22f751ffabf7c5
ElMonoGrande <ian.earnhart@gmail.com>
2025-01-05 02:55:10 -0800

    got rid of stuff

--- build/CMakeFiles/PS.dir/src/collisions.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PS.dir/src/collisions.cpp.o: \
- /home/ian/CODING/C++/particle_simulator/src/collisions.cpp \
- /usr/include/stdc-predef.h
--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o: \
- /home/ian/CODING/C++/particle_simulator/build/_deps/sfml-src/src/SFML/Graphics/BlendMode.cpp \
- /usr/include/stdc-predef.h \
- /home/ian/CODING/C++/particle_simulator/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- /home/ian/CODING/C++/particle_simulator/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/ian/CODING/C++/particle_simulator/build/_deps/sfml-src/include/SFML/Config.hpp
